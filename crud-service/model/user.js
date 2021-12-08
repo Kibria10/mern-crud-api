4216e0e1b198097fde1ccb868d5bffac9d1d8003
Kibria10 <junakkibria56@gmail.com>
2021-12-08 15:00:22 +0600

    updated crud apis

---
+++ crud-service/model/user.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
+
+// how our document look like
+const userSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    url: String,
+});
+
+autoIncrement.initialize(mongoose.connection);
+userSchema.plugin(autoIncrement.plugin, 'user');
+// we need to turn it into a model
+const postUser = mongoose.model('user', userSchema);
+
+export default postUser;
